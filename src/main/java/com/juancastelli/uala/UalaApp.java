c6ef6595da9101859633d78a7589a947ecd469ea
Juan Castelli <juancho_93_22@hotmail.com>
2024-04-03 19:20:20 -0300

    init

---
+++ src/main/java/com/juancastelli/uala/UalaApp.java
@@ -0,0 +1,14 @@
+package com.juancastelli.uala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UalaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UalaApp.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/juancastelli/uala/UalaAppTest.java
@@ -0,0 +1,14 @@
+package com.juancastelli.uala;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UalaAppTest {
+
+    @Test
+    void contextLoads() {
+        assertEquals(true,true);
+    }
+
+}
